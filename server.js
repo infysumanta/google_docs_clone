32a18a9877d69c7fab4c9474b5e63987e579e095
Sumanta Kabiraj <sumantablog@gmail.com>
2022-11-18 01:50:33 +0530

    Socket Connection

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/google_docs", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 const io = require("socket.io")(3001, {
   cors: {
     origin: "*",
@@ -5,4 +12,16 @@ const io = require("socket.io")(3001, {
   },
 });
 
-io.on("connection", (socket) => {});
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.on("get-document", (documentId) => {
+    const data = "";
+    socket.join(documentId);
+
+    socket.emit("load-document", data);
+
+    socket.on("send-changes", (delta) => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
+    });
+  });
+});
